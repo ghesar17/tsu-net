5ec88f0d4f13d6e81a74a0f2c7e06f8536846749
Ghesar <ghesar4phanday@gmail.com>
2024-01-17 16:59:57 -0500

    added form to add community

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 // pages and components
 import Home from "./pages/Home.jsx";
 import PopularPosts from "./pages/PopularPosts.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import CreateCommunity from "./pages/CreateCommunity.jsx";
 
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <div>
-        <BrowserRouter>
-          <Routes>
-            <Route path={"/"} element={<Home />} />
-            <Route path={"/popular"} element={<PopularPosts />} />
-            <Route path={"/not-found"} element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={"/"} element={<Home/>}/>
+                        <Route path={"/create-community"} element={<CreateCommunity/>}/>
+                        <Route path={"/popular"} element={<PopularPosts/>}/>
+                        <Route path={"/not-found"} element={<NotFound/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- client/src/components/MainFeed/MainFeedContainer.jsx
+++ client/src/components/MainFeed/MainFeedContainer.jsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 import PostDetails from "./PostDetails.jsx";
 
 import "../../styles/main-feed-container.css";
 
 const MainFeedContainer = () => {
-  const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("http://localhost:4000/api/posts/");
-      const json = await response.json();
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await fetch("http://localhost:9000/api/posts/");
+            const json = await response.json();
 
-      if (response.ok) {
-        setPosts(json);
-      }
-    };
-    fetchPosts();
-  }, []);
+            if (response.ok) {
+                setPosts(json);
+            }
+        };
+        fetchPosts();
+    }, []);
 
-  if (posts === null) {
-    return <div>Loading...</div>;
-  }
+    if (posts === null) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div className={"main-feed-container"}>
-      {posts && posts.map((post) => <PostDetails key={post._id} post={post} />)}
-    </div>
-  );
+    return (
+        <div className={"main-feed-container"}>
+            {posts && posts.map((post) => <PostDetails key={post._id} post={post}/>)}
+        </div>
+    );
 };
 export default MainFeedContainer;
