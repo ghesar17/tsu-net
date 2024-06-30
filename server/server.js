646f6c0d079809b565802dd7739627d457404040
Ghesar <ghesar4phanday@gmail.com>
2024-06-30 07:31:11 -0400

    server changes

--- server/server.js
+++ server/server.js
@@ -1,27 +1,48 @@
 require("dotenv").config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import path from "path";
 
 const usersRoutes = require("./routes/users");
 const postsRoutes = require("./routes/posts");
 const commentsRoutes = require("./routes/comments");
 const communitiesRouters = require("./routes/communities");
 
+const __dirname = path.dirname(__filename);
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // routes
 app.use("/api/users", usersRoutes);
 app.use("/api/posts", postsRoutes);
 app.use("/api/comments", commentsRoutes);
 app.use("/api/communities", communitiesRouters);
+
+// app.post('/auth/register', register);
+// app.patch('/auth/update/:id', update);
+// app.post('/posts', authenticationMiddleware, createPost);
+// app.post('/auth/login', login);
+// app.post('/auth/register/otp', sendRegistrationMail);
+
+// routes with files attached
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+// file storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
 
 // connect to db
 mongoose
